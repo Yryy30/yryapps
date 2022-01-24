30f4e0500df28b7a7660648f482a688edc7cb915
Yryy30 <mhmd.arr30@gmail.com>
2022-01-24 12:30:19 +0700

    nambah template

---
+++ app/Views/admin/vw_dashboard.php
@@ -0,0 +1,24 @@
+<?= $this->extend('layout/app') ?>
+
+<?= $this->section('content') ?>
+    <header class="page-header">
+        <h2>Dashboard</h2>
+    
+        <div class="right-wrapper pull-right">
+            <ol class="breadcrumbs">
+                <li>
+                    <a href="index.html">
+                        <i class="fa fa-home"></i>
+                    </a>
+                </li>
+                <li><span>Dashboard</span></li>
+            </ol>
+    
+            <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+        </div>
+    </header>
+
+    <div class="row">
+    asda
+    </div>
+<?= $this->endSection() ?>
